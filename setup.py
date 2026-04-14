fbc2bc46fd7e7751aa67b827901d5b39c4c0c931
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2026-04-14 21:50:45 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="musicviz",
-    version="0.0.7",
+    version="0.0.8",
     author="Ashraff Hathibelagal",
     description="A really simple music visualization tool.",
     long_description=long_description,
